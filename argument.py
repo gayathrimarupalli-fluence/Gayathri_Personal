edf126cb9d7ddbcc1f4264d6a7f7a0a250579665
gayathrimarupalli <gayathri@BAN-H7MPXLGWXC.lan>
2024-05-13 20:23:07 +0530

    deleted

--- argument.py
+++
@@ -1,7 +0,0 @@
-def persondetails(Name, **data):
-    print(Name, data)
-    for i, j in data.items():
-        print(Name, i, j)
-    
-
-persondetails(Name='Keerthi', age=35, Location='Bangalore', Mobile=9798354548)
