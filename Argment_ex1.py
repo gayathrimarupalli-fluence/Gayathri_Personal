ca92225d6cc4adc81e0b99ceda5d94c23c3d0478
gayathrimarupalli <gayathrimarupalli@example.com>
2024-05-14 16:39:54 +0530

    added few line

--- Argment_ex1.py
+++ Argment_ex1.py
@@ -1,4 +1,15 @@
-def details(name, age):
-    print(name, age)
+def details(name:'Gayathri', age=34):
+    print(name)
+    print(age)
 
-details('Gayathri', 33)
+details('Keerthi', 35)
+
+def persondetails(name, **data):
+    print(name)
+    print(data)
+    for i, j in data.items():
+        print(name, i, j)
+        print(i,j)
+
+
+persondetails('Gayathri', age=34, Location='Bangalore', No=9844595571)
